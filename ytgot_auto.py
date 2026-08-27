42ce140f6779976b57176a54978b719c498397be
Prembty12 <aksitaabty@gmail.com>
2026-08-27 21:26:39 +0530

    Update ytgot_auto.py

--- ytgot_auto.py
+++ ytgot_auto.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
-def take_ytgot_screenshot(youtube_url):
+def click_and_screenshot(youtube_url):
     print("Browser shuru ho raha hai...")
     
     options = webdriver.ChromeOptions()
@@ -32,12 +32,27 @@ def take_ytgot_screenshot(youtube_url):
         search_box.send_keys(youtube_url)
         print("Link successfully paste ho gaya!")
         
+        time.sleep(2)
+        
+        print("Download button par click kar rahe hain...")
+        try:
+            # Screenshot mein jo 'Download' button dikh raha hai use target karna
+            download_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'Download')]")))
+            driver.execute_script("arguments[0].scrollIntoView(true);", download_btn)
+            time.sleep(1)
+            driver.execute_script("arguments[0].click();", download_btn)
+            print("Download button par click ho gaya!")
+        except Exception as e:
+            print("Button click error:", e)
+        
+        # 10 seconds wait taaki naya page/format load ho jaye
         print("10 seconds wait kar rahe hain...")
         time.sleep(10)
         
+        # Naye page ka screenshot lena
         screenshot_path = "ytgot_screenshot.png"
         driver.save_screenshot(screenshot_path)
-        print(f"\nScreenshot successfully save ho gaya: {screenshot_path}")
+        print(f"\nNaya screenshot successfully save ho gaya: {screenshot_path}")
         
     except Exception as e:
         import traceback
@@ -52,4 +67,4 @@ if __name__ == "__main__":
         link = sys.argv[1]
     else:
         link = input("YouTube ka link yahan paste karein: ")
-    take_ytgot_screenshot(link)
+    click_and_screenshot(link)
