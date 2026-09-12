a40fc9757e1613ebcbf2d06aa28a4ad7cb7e55e9
Prembty12 <aksitaabty@gmail.com>
2026-09-12 22:46:14 +0530

    Increase max_tokens limit from 256 to 2000

--- fallback.sh
+++ fallback.sh
@@ -83,7 +83,7 @@ payload = {
             {'type': 'image_url', 'image_url': {'url': f'data:image/jpeg;base64,{b64_img}'}}
         ]
     }],
-    'max_tokens': 256,
+    'max_tokens': 2000,
     'temperature': 0.4
 }
 
