6d04ae03851a4e5e3fd61c3ea0d8792c163e5e63
Prembty12 <aksitaabty@gmail.com>
2026-09-13 11:50:41 +0530

    Increase max_tokens from 1024 to 2048

--- fallback.sh
+++ fallback.sh
@@ -83,7 +83,7 @@ payload = {
             {'type': 'image_url', 'image_url': {'url': f'data:image/jpeg;base64,{b64_img}'}}
         ]
     }],
-    'max_tokens': 1024,
+    'max_tokens': 2048,
     'temperature': 0.4
 }
 
